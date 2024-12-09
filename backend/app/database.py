1cb91568a5b9fe970b205fc91042d377839e0db1
Stephen Batman <batmania@Stephens-MacBook-Air.local>
2024-12-09 14:47:07 +0900

    chore(be): add logging

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.collection import Collection
+from pymongo.errors import PyMongoError
 from app.config import settings
+import logging
 from typing import Optional
 
+logger = logging.getLogger("DB")
 
 class MongoDB:
     def __init__(self, uri: str, db_name: str):
@@ -14,12 +18,30 @@ class MongoDB:
         try:
             self._client = AsyncIOMotorClient(self._uri)
             self._db = self._client[self._db_name]
+            logger.info(f"Connected {self._uri}, Database: {self._db_name}")
         except Exception as e:
+            logger.error(f"Failed to connect to DB: {str(e)}")
             raise RuntimeError("Failed to connect to DB") from e
 
     async def close(self) -> None:
         if self._client:
             self._client.close()
+            logger.info("DB connection closed.")
 
+    def get_collection(self, name: str) -> Collection:
+        if not self._db:
+            raise RuntimeError("Database is not initialized")
+        return self._db[name]
+
+    async def safe_query(self, query_func):
+        """
+        Wrapper to execute queries with error handling.
+        """
+        try:
+            result = await query_func()
+            return result
+        except PyMongoError as e:
+            logger.error(f"MongoDB query failed: {str(e)}")
+            raise RuntimeError("Database query failed.") from e
 
 mongo = MongoDB(uri=settings.MONGO_URI, db_name=settings.DB_NAME)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.database import mongo
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s | %(message)s",
+)
+logger = logging.getLogger("WaitlistApp")
 
 @asynccontextmanager
 async def app_lifespan(app: FastAPI):
@@ -8,9 +15,11 @@ async def app_lifespan(app: FastAPI):
     Lifespan context manager for managing startup and shutdown events.
     """
     try:
+        logger.info("Starting Waitlist Manager API...")
         await mongo.connect()
         yield
     finally:
+        logger.info("Shutting down Waitlist Manager API...")
         await mongo.close()
 
 app = FastAPI(
@@ -19,4 +28,3 @@ app = FastAPI(
     version="1.0.0",
     lifespan=app_lifespan,
 )
-
